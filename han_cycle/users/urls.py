9a6909091c3458f8cfe2e9624e208e5335ecf6cc
yoonju977 <yoonju977@gmail.com>
2024-07-26 09:17:52 +0900

    4차배포_프로필 수정 나오게 수정

--- han_cycle/users/urls.py
+++ han_cycle/users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from .views import (
+    EditProfileView,
     GoogleCallbackView,
     KakaoLoginView,
     LoginView,
     LogoutView,
     RegisterView,
     UserView,
-    edit_profile,
     googleredirect,
     kakaoredirect,
 )
@@ -15,19 +15,21 @@ from .views import (
 urlpatterns = [
     path("accounts/register", RegisterView.as_view(), name="register"),
     path("accounts/login", LoginView.as_view(), name="login"),
-    path("accounts/user", UserView.as_view(), name="user"),
+    path("accounts/user", UserView.as_view(), name="user"),  # get token by cookie
     path("accounts/logout", LogoutView.as_view(), name="logout"),
     path("accounts/google/login", googleredirect, name="google_login"),
     path(
         "accounts/google/login/callback/",
         GoogleCallbackView.as_view(),
         name="google_callback",
-    ),
+    ),  # Google callback
     path("accounts/kakao/login", kakaoredirect, name="kakao_login"),
     path(
         "accounts/kakao/login/callback/",
         KakaoLoginView.as_view(),
         name="kakao_callback",
     ),
-    path("accounts/profile/edit", edit_profile, name="edit_profile"),
+    path(
+        "accounts/profile/edit", EditProfileView.as_view(), name="edit_profile"
+    ),  # 프로필 수정 엔드포인트
 ]
