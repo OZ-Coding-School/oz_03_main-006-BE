8fb49f5feaa5e6088072cf26922514a05ae1b5cf
yoonju977 <yoonju977@gmail.com>
2024-07-29 17:34:28 +0900

    어드민 비번

--- han_cycle/users/models.py
+++ han_cycle/users/models.py
@@ -6,9 +6,8 @@ from django.utils import timezone
 class User(AbstractUser):
     email = models.EmailField(unique=True)  # Unique
     provider = models.CharField(max_length=100)  # Not null
-    nickname = models.CharField(
-        max_length=100, unique=True
-    )  # Unique, not null
+    nickname = models.CharField(max_length=100, unique=True)  # Unique, not null
+    username = models.CharField(max_length=150, blank=True, null=True, unique=True)
     created_at = models.DateTimeField(
         default=timezone.now
     )  # Timestamp with default value of now
