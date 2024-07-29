a57629ef253e45491e0c2e2bd362e839ed74e23e
https://github.com/siangit/homework.git <ljin6956@gmail.com>
2024-07-29 19:44:39 +0900

    admin login

--- han_cycle/users/models.py
+++ han_cycle/users/models.py
@@ -1,19 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
-
+from .managers import CustomUserManager  # Import the custom manager
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)  # Unique
     provider = models.CharField(max_length=100)  # Not null
     nickname = models.CharField(max_length=100, unique=True)  # Unique, not null
     username = models.CharField(max_length=150, blank=True, null=True, unique=True)
-    created_at = models.DateTimeField(
-        default=timezone.now
-    )  # Timestamp with default value of now
-    profile_image = models.ImageField(
-        upload_to="profile_images/", null=True, blank=True
-    )
+    created_at = models.DateTimeField(default=timezone.now)  # Timestamp with default value of now
+    profile_image = models.ImageField(upload_to="profile_images/", null=True, blank=True)
 
     USERNAME_FIELD = 'nickname'
-    REQUIRED_FIELDS = []  # Corrected this to plural
+    REQUIRED_FIELDS = ['email']  # Added email to required fields
+
+    objects = CustomUserManager()  # Set the custom manager
