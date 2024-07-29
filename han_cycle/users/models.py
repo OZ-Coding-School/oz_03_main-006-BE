3e6824806f49193bf4ff23fd582c51493a4c07ca
yoonju977 <yoonju977@gmail.com>
2024-07-29 15:19:07 +0900

    유저네임필드 닉네임으로 변경

--- han_cycle/users/models.py
+++ han_cycle/users/models.py
@@ -6,7 +6,9 @@ from django.utils import timezone
 class User(AbstractUser):
     email = models.EmailField(unique=True)  # Unique
     provider = models.CharField(max_length=100)  # Not null
-    nickname = models.CharField(max_length=100)  # Unique, not null
+    nickname = models.CharField(
+        max_length=100, unique=True
+    )  # Unique, not null
     created_at = models.DateTimeField(
         default=timezone.now
     )  # Timestamp with default value of now
@@ -14,5 +16,5 @@ class User(AbstractUser):
         upload_to="profile_images/", null=True, blank=True
     )
 
-    # USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "nickname"
     REQUIRED_FIELDS = []  # Corrected this to plural
