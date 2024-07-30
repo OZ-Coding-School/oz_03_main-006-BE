6f6145ebb260f95e62054c1c66ee5e960914e3c3
yoonju977 <yoonju977@gmail.com>
2024-07-30 14:50:41 +0900

    최대페이지 갯수 80개

--- han_cycle/boards/pagination.py
+++ han_cycle/boards/pagination.py
@@ -4,8 +4,12 @@ from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
     page_size = 8  # 한페이지에 8개의 게시물
+    max_page_size = 8 * 80  # 총 페이지 수를 80개로 제한 (한 페이지에 8개의 게시물 기준)
 
     def get_paginated_response(self, data):
+        # 총 페이지 수가 80개를 초과하지 않도록 제한
+        total_pages = min(self.page.paginator.num_pages, 80)
+
         return Response(
             {
                 "links": {
@@ -13,7 +17,7 @@ class CustomPagination(PageNumberPagination):
                     "previous": self.get_previous_link(),
                 },
                 "count": self.page.paginator.count,
-                "total_pages": self.page.paginator.num_pages,  # 총 페이지 수
+                "total_pages": total_pages,  # 총 페이지 수
                 "page_size": self.page_size,
                 "results": data,
             }
