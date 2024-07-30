27a2f0c666804ee907ffe2fa8d515b836f502814
yoonju977 <yoonju977@gmail.com>
2024-07-30 14:57:38 +0900

    임서연연pr반영

--- han_cycle/boards/models.py
+++ han_cycle/boards/models.py
@@ -18,7 +18,7 @@ def image_upload_path(instance, filename):
 class Post(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=200, null=False)
-    tag = models.CharField(max_length=200, null=False)
+    tag = models.CharField(max_length=200, null=True, blank=True)
     location = models.ForeignKey(
         Location, on_delete=models.CASCADE, related_name="posts"
     )  # location 필드 추가
